650acb22a4fa8704dc577c91126894bee3d4e358
Rajasree Jayaram <37769172+rajashree921@users.noreply.github.com>
2020-02-26 16:05:48 +0530

    starter flask app

---
+++ flask_code/hello.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request
+import cv2
+import os
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+
+@app.route('/')
+def upload_file():
+   return render_template('index.html')
+	
+@app.route('/uploader', methods = ['GET', 'POST'])
+def upload_file1():
+   if request.method == 'POST':
+      f = request.files['file']
+      filename = "static/"+secure_filename(f.filename)
+      f.save(filename)
+    #   os.remove(filename)
+      return 'file uploaded successfully<br><img src='+filename+'>'
+		
+if __name__ == '__main__':
+   app.run()
