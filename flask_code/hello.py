7c72a74496f716dca76aa6e6e1c8da075b4624c4
rajashree921 <rajashree921@gmail.com>
2020-02-27 15:22:04 +0530

    trial predict page with bokeh and loader

--- flask_code/hello.py
+++ flask_code/hello.py
@@ -2,6 +2,13 @@ from flask import Flask, render_template, request
 # import cv2
 import os
 from werkzeug.utils import secure_filename
+# from keras.preprocessing import image
+# from keras.applications.inception_v3 import InceptionV3, preprocess_input
+# import numpy as np
+# import PIL
+from bokeh.plotting import figure
+from bokeh.embed import components
+from numpy import pi
 
 app = Flask(__name__)
 
@@ -13,10 +20,49 @@ def upload_file():
 def upload_file1():
    if request.method == 'POST':
       f = request.files['file']
-      filename = "static/"+secure_filename(f.filename)
+      filename = "static/uploaded/"+secure_filename(f.filename)
       f.save(filename)
-    #   os.remove(filename)
+      # os.remove(filename)
+      # img = image.load_img(filename, target_size=(299, 299))
+      # x = image.img_to_array(img)
+      # x = np.expand_dims(x, axis=0)
+      # x = preprocess_input(x)
       return render_template('predict.html',fn = filename)
-		
+
+from flask import request
+def shutdown_server():
+    func = request.environ.get('werkzeug.server.shutdown')
+    if func is None:
+        raise RuntimeError('Not running with the Werkzeug Server')
+    func()
+
+@app.route('/shutdown', methods=['GET'])
+def shutdown():
+    shutdown_server()
+    return 'Server shutting down...'  
+
+IMAGE_LABELS = ['Headphone', 'Mouse', 'Camera', 'Smartphone',
+                'Glasses', 'Shoes', 'Watch', 'Laptop']
+
+def generate_barplot(predictions):
+    """ Generates script and `div` element of bar plot of predictions using
+    Bokeh
+    """
+    plot = figure(y_range=IMAGE_LABELS, plot_height=300)
+    plot.hbar(y=IMAGE_LABELS, right=predictions, height=0.6)
+    plot.yaxis.major_label_orientation = pi / 2.
+    plot.sizing_mode = 'scale_width'
+    return components(plot)
+
+@app.route('/predict1')
+def predict():
+    predictions = [0.1, 0.5, 0.3, 0.75, 0.9, 0.5, 0.1, 0.0]
+    script, div = generate_barplot(predictions)
+    return render_template(
+        'predict1.html',
+        plot_script=script,
+        plot_div=div,
+    )        
+
 if __name__ == '__main__':
-   app.run()
+   app.run(debug=True)
